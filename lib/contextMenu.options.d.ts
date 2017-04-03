5e27d12e1cfe1b52add6ef3e60fbcbf40000cc90
Isaac Mann <isaacplmann@gmail.com>
2017-04-03 09:34:12 -0400

    Updates

--- lib/contextMenu.options.d.ts
+++
@@ -1,5 +0,0 @@
-import { OpaqueToken } from '@angular/core';
-export interface IContextMenuOptions {
-    useBootstrap4: boolean;
-}
-export declare const CONTEXT_MENU_OPTIONS: OpaqueToken;
--- lib/ngx-contextmenu.d.ts
+++
@@ -1,6 +0,0 @@
-import { IContextMenuOptions } from './contextMenu.options';
-import { ModuleWithProviders } from '@angular/core';
-export declare class ContextMenuModule {
-    static forRoot(options: IContextMenuOptions): ModuleWithProviders;
-}
-export default ContextMenuModule;
