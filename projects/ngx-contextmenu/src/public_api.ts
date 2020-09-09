bf2453e2181273a0f9761acb187351f60be2f779
Marco Santarelli <marco@backbase.com>
2020-09-09 09:02:58 +0200

    Match new project schematics structure

--- projects/ngx-contextmenu/src/public_api.ts
+++ projects/ngx-contextmenu/src/public_api.ts
@@ -4,6 +4,8 @@
 
 export { ContextMenuModule } from './lib/ngx-contextmenu';
 
+export * from './lib/contextMenu.attach.directive';
 export * from './lib/contextMenu.component';
+export * from './lib/contextMenu.item.directive';
 export * from './lib/contextMenu.service';
 export * from './lib/contextMenu.options';
