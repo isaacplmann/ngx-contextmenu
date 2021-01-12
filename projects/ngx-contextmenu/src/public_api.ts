a1abef8e6c4a8babc5390c3c44dd924bb8784f0d
ASheng <oiqwys@gmail.com>
2021-01-12 11:06:13 +0800

    upgrade to angular 10 (#214)
    * upgrade angular to 10
    
    * upgrade cdk
    
    * finish upgrade to angular 10
    
    * add change log and readme
    
    * core-js polyfills

--- projects/ngx-contextmenu/src/public_api.ts
+++ projects/ngx-contextmenu/src/public_api.ts
@@ -7,3 +7,5 @@ export { ContextMenuModule } from './lib/ngx-contextmenu';
 export * from './lib/contextMenu.component';
 export * from './lib/contextMenu.service';
 export * from './lib/contextMenu.options';
+export * from './lib/contextMenu.attach.directive';
+export * from './lib/contextMenu.item.directive';
